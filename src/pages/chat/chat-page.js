80caca59abdd1868e211e4eebf401f1625f32d7b
Stanley Hayford <hayfordstanley@gmail.com>
2020-09-07 19:49:05 +0000

    acquiring query string

--- src/pages/chat/chat-page.js
+++ src/pages/chat/chat-page.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./chat.css";
+import queryString from "query-string";
 
-const ChatPage = () => {
+const ChatPage = ({location}) => {
+    const [name, setName] = useState("");
+    const [room, setRoom] = useState("");
+
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search);
+        setRoom(room);
+        setName(name);
+    }, [location.search]);
     return (
         <div>
             <h1>Chat Page</h1>
