ac7c0916ae8464373fbddd1feabb9aecaf00ac21
Stanley Hayford <hayfordstanley@gmail.com>
2020-09-09 11:23:42 +0000

    endpoint changed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const SERVER_ENDPOINT = 'localhost:5000';
+export const SERVER_ENDPOINT = 'https://chat-app-stanley.herokuapp.com/ ';
 export const EMIT_JOIN = 'join';
 export const EMIT_DISCONNECT = 'disconnect';
